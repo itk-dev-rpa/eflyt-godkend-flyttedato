4c136c698f058b5b81da1d7fdefcef9691b1d996
kri-bak <kriba@aarhus.dk>
2024-09-13 10:06:35 +0200

    moved functions to shared_components and and adjusted the main file

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -5,8 +5,9 @@ from datetime import date
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConnection
-from itk_dev_shared_components.eflyt import eflyt_login, eflyt_search
+from itk_dev_shared_components.eflyt import eflyt_login, eflyt_search, eflyt_case
 from itk_dev_shared_components.eflyt.eflyt_case import Case
 
 from robot_framework import config
@@ -23,7 +24,10 @@ def process(orchestrator_connection: OrchestratorConnection) -> None:
     cases = filter_cases(cases)
     for case in cases:
         eflyt_search.open_case(browser, case.case_number)
-        handle_case(browser)
+        try:
+            handle_case(browser)
+        except NoSuchElementException as e:
+            orchestrator_connection.log_error(e.msg)
 
 
 def filter_cases(cases: list[Case]) -> list[Case]:
@@ -43,7 +47,7 @@ def filter_cases(cases: list[Case]) -> list[Case]:
 
     filtered_cases = []
     for case in cases:
-        if case.case_worker[:2].upper() == "EF" and case.case_types in approved_case_types:
+        if case.case_worker[:2].upper() == "EF" and case.case_worker[2:].isdigit() and len(case.case_types) == 1 and case.case_type[0] in approved_case_types:
             filtered_cases.append(case)
     return filtered_cases
 
@@ -56,34 +60,16 @@ def handle_case(browser: webdriver.Chrome):
         case: A case that is currently open in the browser.
     """
     registered_date = browser.find_element(By.ID, "ctl00_ContentPlaceHolder2_GridViewMovingPersons_ctl02_lnkDateCPR").text
-    browser.find_element(By.ID, "ctl00_ContentPlaceHolder2_ptFanePerson_moPersonTab_gvManuelOpfolgning_ctl05_lbtnVisSvar").click()
-    response_date = browser.find_element(By.ID, "ctl00_ContentPlaceHolder2_ptFanePerson_moPersonTab_txtFradato")
-
+    browser.find_element(By.LINK_TEXT, "Vis svar").click()
+    # browser.find_element(By.ID, "ctl00_ContentPlaceHolder2_ptFanePerson_moPersonTab_gvManuelOpfolgning_ctl05_lbtnVisSvar").click()
+    response_date = browser.find_element(By.ID, "ctl00_ContentPlaceHolder2_ptFanePerson_moPersonTab_txtFradato").get_attribute("value")
     browser.find_element(By.ID, "ctl00_ContentPlaceHolder2_ptFanePerson_moPersonTab_btnLogivartResponseLuk").click()
 
     if registered_date == response_date:  # Approve the case
-        deadline_field = browser.find_element(By.ID, "ctl00_ContentPlaceHolder2_ptFanePerson_ncPersonTab_txtDeadline")
-        deadline_field.clear()
-        note_field = browser.find_element(By.ID, "ctl00_ContentPlaceHolder2_ptFanePerson_ncPersonTab_txtDeadlineNote")
-        note_field.clear()
-
-        browser.find_element(By.ID, "ctl00_ContentPlaceHolder2_ptFanePerson_stcPersonTab1_btnGodkend").click()
-        browser.find_element(By.ID, "ctl00_ContentPlaceHolder2_ptFanePerson_stcPersonTab1_btnApproveYes").click()
-
-        person_table = browser.find_element(By.ID, "ctl00_ContentPlaceHolder2_GridViewMovingPersons")
-        rows = person_table.find_elements(By.TAG_NAME, "tr")
-        rows.pop(0)
-        approve_persons_button = browser.find_element(By.ID, "ctl00_ContentPlaceHolder2_ptFanePerson_stcPersonTab1_btnGodkendAlle")
-
-        if not approve_persons_button.is_enabled():
-            for row in rows:
-                row.find_element(By.XPATH, "td[2]").click()
-                browser.find_element(By.ID, "ctl00_ContentPlaceHolder2_ptFanePerson_stcPersonTab1_btnGodkend").click()
-                approve_button = browser.find_element(By.ID, "ctl00_ContentPlaceHolder2_ptFanePerson_stcPersonTab1_btnApproveYes")
-                if approve_button.is_displayed():
-                    approve_button.click()
-        else:
-            approve_persons_button.click()
+        eflyt_case.approve_case(browser)
+        if not eflyt_case.check_all_approved(browser):
+            raise RuntimeError("An error occurred during case approval.")
+        eflyt_case.add_note(browser, "Datoer stemmer overens, flytning godkendt.")
 
 
 if __name__ == '__main__':
