8adab18708329c93dcf944a289514bc8ee4bfaa0
Mathias G <ghbm@aarhus.dk>
2026-04-28 11:15:05 +0200

    Remove `if __name__ == "__main__"` blocks

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -108,10 +108,3 @@ def handle_case(browser: webdriver.Chrome, case: Case) -> bool:
         eflyt_case.add_note(browser, note_text)
         return True
     return False
-
-
-if __name__ == '__main__':
-    conn_string = os.getenv("OpenOrchestratorConnString")
-    crypto_key = os.getenv("OpenOrchestratorKey")
-    oc = OrchestratorConnection("Eflyt Test", conn_string, crypto_key, "", '', "")
-    process(oc)
